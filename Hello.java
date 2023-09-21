ba1546105000ac6e0d9c4f17cea05e3301d19b8d
nguyenmaixuanhuong <xuanhuong22211@gmail.com>
2023-09-21 16:44:36 +0700

    update

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
 public class Hello{
     public static void main(String []args){
-<<<<<<< HEAD
-        System.out.println("hello - B2014572");
-=======
-        System.out.println("hello Huong");
->>>>>>> hotfix
+        System.out.println("hello");      
     }
 }
